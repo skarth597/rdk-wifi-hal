a1f71baa619d054cc20c4ad85e799b8851bf3760
Amalesh Nandh <Amalesh_Nandh@Comcast.com>
2025-04-02 13:58:06 +0000

    RDKB-12345678 Add WPA3-Compatibility for 6GHz
    Impacted Platforms:
    Tech-XB10
    
    Reason for change: To add WPA3-Personal-Compatibility mode in onewifi and rdk-wifi-hal
    
    Test Procedure: Enable WPA3-Compatibility RFC and set security mode as
                    WPA3-Personal-Compatibility.
                    Check Client connectivity and internet
    
    Risks: Medium
    Priority:P1
    
    Signed-off-by:Amalesh_Nandh@comcast.com

--- src/wifi_hal_hostapd.c
+++ src/wifi_hal_hostapd.c
@@ -490,6 +490,12 @@ int update_security_config(wifi_vap_security_t *sec, struct hostapd_bss_config *
 #if HOSTAPD_VERSION >= 210
     conf->wpa_key_mgmt_rsno = 0;
 #endif /* HOSTAPD_VERSION >= 210 */
+
+#if defined(CONFIG_IEEE80211BE)
+    conf->wpa_key_mgmt_rsno_2 = 0;
+    conf->rsn_pairwise_rsno_2 = 0;
+#endif /* CONFIG_IEEE80211BE */
+
     conf->wpa = 0;
     memset(&test_ip, 0, sizeof(test_ip));
 
@@ -559,7 +565,15 @@ int update_security_config(wifi_vap_security_t *sec, struct hostapd_bss_config *
             conf->wpa_key_mgmt = WPA_KEY_MGMT_PSK;
 #if HOSTAPD_VERSION >= 210
             conf->wpa_key_mgmt_rsno = WPA_KEY_MGMT_SAE;
-            conf->sae_pwe = 1;
+#ifdef CONFIG_IEEE80211BE
+            if(is_wifi_hal_6g_radio_from_interfacename(conf->iface) == true) {
+                conf->wpa_key_mgmt = WPA_KEY_MGMT_SAE;
+                conf->wpa_key_mgmt_rsno = 0;
+                conf->wpa_key_mgmt_rsno_2 = WPA_KEY_MGMT_SAE_EXT_KEY;
+                conf->rsn_pairwise_rsno_2 = WPA_CIPHER_GCMP_256;
+            }
+#endif /* CONFIG_IEEE80211BE*/
+            conf->sae_pwe = 2;
 #endif /* HOSTAPD_VERSION >= 210 */
             conf->auth_algs = WPA_AUTH_ALG_SAE | WPA_AUTH_ALG_SHARED | WPA_AUTH_ALG_OPEN;
             break;
@@ -618,6 +632,11 @@ int update_security_config(wifi_vap_security_t *sec, struct hostapd_bss_config *
         conf->ieee80211w_rsno = (enum mfp_options) MGMT_FRAME_PROTECTION_REQUIRED;
 #endif /* HOSTAPD_VERSION >= 210 */
         conf->sae_require_mfp = 1;
+#if defined(CONFIG_IEEE80211BE)
+        if(is_wifi_hal_6g_radio_from_interfacename(conf->iface) == true) {
+            conf->ieee80211w = (enum mfp_options) MGMT_FRAME_PROTECTION_REQUIRED;
+        }
+#endif /* CONFIG_IEEE80211BE */
     }
 #endif
 
