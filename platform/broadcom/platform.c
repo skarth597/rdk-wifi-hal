1babc3917a5c299b9ff36797dddef40c50f82d17
Ben Carlson <b.carlson@cablelabs.com>
2025-04-10 14:10:08 -0700

    Added `wifi_sendActionFrameExt` implementations for all platforms.
    Signed-off-by: Ben Carlson <b.carlson@cablelabs.com>

--- platform/broadcom/platform.c
+++ platform/broadcom/platform.c
@@ -507,10 +507,15 @@ int platform_get_chanspec_list(unsigned int radioIndex, wifi_channelBandwidth_t 
     return RETURN_OK;
 }
 
-int wifi_sendActionFrameExt(INT apIndex, mac_address_t MacAddr, UINT frequency, UINT wait, UCHAR *frame, UINT len)
+INT wifi_sendActionFrameExt(INT apIndex, mac_address_t MacAddr, UINT frequency, UINT wait, UCHAR *frame, UINT len)
 {
     int res = wifi_hal_send_mgmt_frame(apIndex, MacAddr, frame, len, frequency, wait);
-    return (res == 0) ? RETURN_OK : RETURN_ERR;
+    return (res == 0) ? WIFI_HAL_SUCCESS : WIFI_HAL_ERROR;
+}
+
+INT wifi_sendActionFrame(INT apIndex, mac_address_t MacAddr, UINT frequency, UCHAR *frame, UINT len)
+{
+    return wifi_sendActionFrameExt(apIndex, MacAddr, frequency, 0, frame, len);
 }
 
 int platform_set_acs_exclusion_list(unsigned int radioIndex, char* str)
--- platform/qualcomm/platform_xer5.c
+++ platform/qualcomm/platform_xer5.c
@@ -1713,3 +1713,14 @@ INT platform_create_interface_attributes(struct nl_msg **msg_ptr, wifi_radio_inf
     }
     return RETURN_OK;
 }
+
+INT wifi_sendActionFrameExt(INT apIndex, mac_address_t MacAddr, UINT frequency, UINT wait, UCHAR *frame, UINT len)
+{
+    int res = wifi_hal_send_mgmt_frame(apIndex, MacAddr, frame, len, frequency, wait);
+    return (res == 0) ? WIFI_HAL_SUCCESS : WIFI_HAL_ERROR;
+}
+
+INT wifi_sendActionFrame(INT apIndex, mac_address_t MacAddr, UINT frequency, UCHAR *frame, UINT len)
+{
+    return wifi_sendActionFrameExt(apIndex, MacAddr, frequency, 0, frame, len);
+}
