5a31a61ed49b525e57c74cd4eb9f3159332aa4bc
Narendra Varma Dandu <narendandu@gmail.com>
2025-05-08 13:19:38 -0700

    RDKB-57868, RDKB-57741 Extend webconfig_subdoc_type_associated_clients to include Association/Re-Association request frame from clients. (#192)
    * RDKB-57741: Send Assoc frame data in associated clients subdoc
    
    Association/Reassociation frame data is encoded into a hex string
    and sent as part of Associated Clients subdoc for the easy mesh
    agent consumption.
    
    Reason for change: Easymesh agent to be aware of associated client
    data.
    Test Procedure: Ensured that appropriate hex string is being sent
    when assoc frame is received.
    Risks: Low
    Priority: P0
    
    Change-Id: Ibe3ee428649efa6d925b9349e98052e28afd5577
    Signed-off-by: Amarnath Hullur Subramanyam <amarnath_hullursubramanyam@comcast.com>
    
    * RDKB-57868: Return appropriate error/success return value for switch channel.
    
    Reason for change: Due to incorrect return value OneWifi was not retrying
    channel change when the failure happened. This commit is to ensure that
    wifi-hal returns appropriate value. In case of failure retval, OneWifi
    can retry the channel change.
    
    Test Procedure: Ensured that the config change is reflected in radio
    subdoc as part of successful channel change request to agent.
    
    Risks: Low
    Priority: P1
    Signed-off-by: Amarnath Hullur Subramanyam <amarnath_hullursubramanyam@comcast.com>
    Change-Id: I57e4fd54591c82673dbf6ce2212ef929b4e7f04b
    
    * RDKB-57868: Ignore error of switch channel in case of non-first interface
    
    Reason for change: In some platform, switch channel for non-first interface
    can return error due to CSA in progress of first interface. So, ignore the
    error of switch channel on non-first interface.
    
    Test Procedure: Ensured that the config change is reflected in radio
    subdoc as part of successful channel change request to agent.
    
    Risks: Low
    Priority: P1
    Signed-off-by: Amarnath Hullur Subramanyam <amarnath_hullursubramanyam@comcast.com>
    Change-Id: If1ac7d077a8179b9586f1ee8b57020d06e711f6e
    
    ---------
    
    Signed-off-by: Amarnath Hullur Subramanyam <amarnath_hullursubramanyam@comcast.com>
    Co-authored-by: Amarnath Hullur Subramanyam <amarnath.hs@gmail.com>
    Co-authored-by: Amarnath Hullur Subramanyam <amarnath_hullursubramanyam@comcast.com>

--- src/wifi_hal_nl80211.c
+++ src/wifi_hal_nl80211.c
@@ -5785,7 +5785,8 @@ int nl80211_switch_channel(wifi_radio_info_t *radio)
     int sec_chan_offset, freq, freq1, bandwidth;
     u8 seg0;
     char country[8];
-    int ret;
+    int ret = 0;
+    bool is_first_interface;
 
     param = &radio->oper_param;
     get_coutry_str_from_code(param->countryCode, country);
@@ -5862,6 +5863,7 @@ int nl80211_switch_channel(wifi_radio_info_t *radio)
     wifi_hal_dbg_print("%s:%d chan_freq: %d center_freq: %d bandwidth: %d sec_chan_offset: %d\n",
         __func__, __LINE__, freq, freq1, bandwidth, sec_chan_offset);
 
+    is_first_interface = true;
     hash_map_foreach(radio->interface_map, interface) {
         if (!interface->bss_started) {
             continue;
@@ -5874,12 +5876,15 @@ int nl80211_switch_channel(wifi_radio_info_t *radio)
         ret = hostapd_switch_channel(&interface->u.ap.hapd, &csa_settings);
         pthread_mutex_unlock(&g_wifi_hal.hapd_lock);
 
-        if (ret != 0) {
+        /* Ignore the error if the error is not on first interface,
+           as CSA would be in progress after the first interface channel switch. */
+        if (ret != 0 && is_first_interface == true) {
             wifi_hal_error_print("%s:%d interface: %s failed to switch channel to %d, error: %d\n",
                 __func__, __LINE__, interface->name, param->channel, ret);
+            return ret;
         }
+        is_first_interface = false;
     }
-
     return 0;
 }
 
