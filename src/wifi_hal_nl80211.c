cadadaa52712ded52dd9c7a365a1de89e35c5379
poornakumar-pk <poornakumar_mezhiselvam@comcast.com>
2025-05-21 14:37:16 +0530

    BCOMB-2722  Additional changes for build failure (#201)
    Reason for change: Additional changes for build failure
    Test Procedure: Build should be successfully built
    Risks: None
    Priority: P2
    Signed-off-by: poornakumar_mezhiselvam@comcast.com

--- src/wifi_hal_nl80211.c
+++ src/wifi_hal_nl80211.c
@@ -2119,7 +2119,6 @@ int process_mgmt_frame(struct nl_msg *msg, void *arg)
 {
     wifi_interface_info_t *interface;
     struct genlmsghdr *gnlh;
-    unsigned short fc, stype;
     struct nlattr *tb[NL80211_ATTR_MAX + 1], *attr;
     unsigned int len;
     struct ieee80211_mgmt *mgmt = NULL;
@@ -2201,6 +2200,7 @@ int process_mgmt_frame(struct nl_msg *msg, void *arg)
     }
 #if  (defined(TCXB7_PORT) || defined(CMXB7_PORT) || defined(TCXB8_PORT) || defined(TCHCBRV2_PORT) || defined(XB10_PORT) || defined(SCXER10_PORT) || defined(VNTXER5_PORT) || defined(TARGET_GEMINI7_2))
     if (tb[NL80211_ATTR_RX_PHY_RATE_INFO]) {
+	unsigned short fc, stype;
         phy_rate = nla_get_u32(tb[NL80211_ATTR_RX_PHY_RATE_INFO]) *10;
 	fc = le_to_host16(mgmt->frame_control);
         stype = WLAN_FC_GET_STYPE(fc);
