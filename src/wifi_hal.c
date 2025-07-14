6a6142e992f492572cfb17fac9b1482c6eb789a7
Samyuktha Karthikeyan <samyuktha_karthikeyan@comcast.com>
2025-07-14 11:36:59 +0000

    RDKB-59758 No secondary channel neighbors in the on-channel scan results
    Reason for change: No secondary channel neighbors in the on-channel scan results are reported
    Test Procedure:
    1.Flash the provided fix build in XB10
    2.Enable debug log touch /nvram/wifiMonDbg
    3.Check the current operating channel of XB10 and make sure there's at least one active neighboring AP on one of the secondary channels
    4.Check wifiMon log for on-channel scans. Observe the neighbors found.
    Priority: P1
    Risks: Low
    Signed-off-by: Samyuktha Karthikeyan samyuktha_karthikeyan@comcast.com

--- src/wifi_hal.c
+++ src/wifi_hal.c
@@ -3079,14 +3079,13 @@ INT wifi_hal_startNeighborScan(INT apIndex, wifi_neighborScanMode_t scan_mode, I
         pthread_mutex_lock(&interface->scan_state_mutex);
         interface->scan_state = WIFI_SCAN_STATE_STARTED;
         pthread_mutex_unlock(&interface->scan_state_mutex);
+
+        // - scan_state is changed by nl80211_get_scan_results()
+        if (nl80211_get_scan_results(interface) != RETURN_OK)
+            return WIFI_HAL_ERROR;
+
+        return WIFI_HAL_SUCCESS;
     }
-
-    // - scan_state is changed by nl80211_get_scan_results()
-    if (nl80211_get_scan_results(interface) != RETURN_OK)
-        return WIFI_HAL_ERROR;
-
-    return WIFI_HAL_SUCCESS;
-}
 
     wifi_hal_stats_dbg_print("%s:%d: [SCAN] oper_param.opclass:%d, oper_param.channel:%d\n",
         __func__, __LINE__, radio->oper_param.operatingClass, radio->oper_param.channel);
