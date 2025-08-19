6ab26cc0a1d98c9a03e02f5e95f200082161ebe3
Jayanthrajan <jayanth.rajan@sky.uk>
2025-08-19 02:27:32 +0100

    RDKB-59636: CSA frame won't be triggered during next optimization after radar detection. (#309)
    Reason for change: PHY is getting reconfigured due to struct mismatch.
    Test Procedure:channel change should trigger CSA.
    Risks: Low
    Priority: P1
    
    Signed-off-by: Jayanth Rajan <jayanth.rajan@sky.uk>

--- src/wifi_hal.c
+++ src/wifi_hal.c
@@ -926,7 +926,10 @@ INT wifi_hal_setRadioOperatingParameters(wifi_radio_index_t index, wifi_radio_op
         radio->oper_param.operatingClass = operationParam->operatingClass;
         radio->oper_param.channelWidth = operationParam->channelWidth;
         radio->oper_param.autoChannelEnabled = operationParam->autoChannelEnabled;
-        radio->oper_param.DfsEnabledBootup = operationParam->DfsEnabledBootup;
+		radio->oper_param.DfsEnabledBootup = operationParam->DfsEnabledBootup;
+		strncpy(radio->oper_param.radarDetected, operationParam->radarDetected,
+				sizeof(radio->oper_param.radarDetected)-1);
+		radio->oper_param.DFSTimer = operationParam->DFSTimer;
         memcpy(radio->oper_param.channel_map, operationParam->channel_map,
             sizeof(radio->oper_param.channel_map));
 
